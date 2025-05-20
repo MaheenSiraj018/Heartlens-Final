8e2f55d9023175b50b07c63bce02f3421b5e2dac
Maheen Siraj <113521903+MaheenSiraj018@users.noreply.github.com>
2025-05-20 23:09:13 +0500

    final commit

--- lib/data/student.dart
+++ lib/data/student.dart
@@ -1,17 +1,3 @@
-// class Student {
-//   String name;
-//   int cat1;
-//   int cat2;
-//   int endterm;
-//
-//   Student({
-//     required this.name,
-//     required this.cat1,
-//     required this.cat2,
-//     required this.endterm,
-//   });
-// }
-
 
 class Student {
 String name;
